5b86364df2e6f6120e660ed646f371ad6f8feeb4
tarabarico <tarabarico@gmail.com>
2022-07-23 17:14:59 -0400

    Reworked db backend. Added Inventory class.

---
+++ src/main/java/dev/tarico/models/Inventory.java
@@ -0,0 +1,165 @@
+package dev.tarico.models;
+
+import dev.tarico.models.helpers.ValueChecker;
+
+/**
+ * Inventory keeps track of a single item, its location, and quantity.
+ * 
+ * @author Tara Arico - 7.23.2022
+ */
+public class Inventory {
+    private int inventoryId;
+    private int itemId;
+    private int locationId;
+    private int quantity;
+    private boolean valid = false;
+
+    /**
+     * Creates an Inveotry object with the specified parameters.
+     * 
+     * @param inventoryId - id of this Inventory object. Must not be negative.
+     * @param itemId - id of the item stored in this Inventory object. Must not be negative.
+     * @param locationId - id of the Inventory's location. Must not be negative.
+     * @param quantity - amount of items being stored at this location. Must not be negative.
+     */
+    public Inventory(int inventoryId, int itemId, int locationId, int quantity) {
+        try {
+            ValueChecker.checkNegative(inventoryId, "id");
+            this.inventoryId = inventoryId;
+            ValueChecker.checkNegative(itemId, "id");
+            this.itemId = itemId;
+            ValueChecker.checkNegative(locationId, "id");
+            this.locationId = locationId;
+            ValueChecker.checkNegative(quantity, "quantity");
+            this.quantity = quantity;
+            this.valid = true;
+        } catch(IllegalArgumentException e) { 
+            this.valid = false;
+        }
+    }
+
+    /**
+     * Creates an Inventory object with the specified parameters. Using this constructor will 
+     * make 0 the default quantity of the item specified.
+     * 
+     * @param inventoryId - id of this Inventory object. Must not be negative.
+     * @param itemId - id of the item stored in this Inventory object. Must not be negative.
+     * @param locationId - id of the Inventory's location. Must not be negative.
+     */
+    public Inventory(int inventoryId, int itemId, int locationId) {
+        this(inventoryId, itemId, locationId, 0);
+    }
+
+    /**
+     * Retrieves the id of the object.
+     * 
+     * @return id of this Inventory object
+     */
+    public int getInventoryId() {
+        return inventoryId;
+    }
+
+    /**
+     * Sets the id of this Inventory object to the argument specified.
+     * 
+     * @param inventoryId - id to set. Must not be negative.
+     */
+    public void setInventoryId(int inventoryId) {
+        try {
+            ValueChecker.checkNegative(inventoryId, "id");
+            this.inventoryId = inventoryId;
+        } catch (IllegalArgumentException e) {
+            //silently fail for now
+        }
+    }
+    
+    /**
+     * Retrieves the id of the Inventory's item.
+     * 
+     * @return id of item
+     */
+    public int getItemId() {
+        return itemId;
+    }
+
+    /**
+     * Sets the id of the item in this Inventory object to the argument specified.
+     * 
+     * @param itemID - id to set. Must not be negative
+     */
+    public void setItemId(int itemId) {
+        try {
+            ValueChecker.checkNegative(itemId, "id");
+            this.itemId = itemId;
+        } catch (IllegalArgumentException e) {
+            //silently fail for now
+        }
+    }
+
+    /**
+     * Retrieves the id of this Inventory's location.
+     * 
+     * @return the id of the location
+     */
+    public int getLocationId() {
+        return locationId;
+    }
+
+    /**
+     * Sets the Inventory's location id to the argument specified.
+     * 
+     * @param locationId - id of location to set. Must not be negative.
+     */
+    public void setLocationId(int locationId) {
+        try {
+            ValueChecker.checkNegative(locationId, "id");
+            this.locationId = locationId;
+        } catch (IllegalArgumentException e) { 
+            //silently fail for now
+        }
+    }
+
+    /**
+     * Retrieves the amount of items stored in this Inventory object.
+     * 
+     * @return quantity of items stored here
+     */
+    public int getQuantity() {
+        return quantity;
+    }
+
+    /**
+     * Sets the amount of items stored in this Inventory object to the argument specified.
+     * 
+     * @param quantity - quantity of items. Must not be negative.
+     */
+    public void setQuantity(int quantity) {
+        try {
+            ValueChecker.checkNegative(quantity, "quantity");
+            this.quantity = quantity;
+        } catch (IllegalArgumentException e) {
+            // silently fail for now
+        }
+    }
+
+    /**
+     * Returns whether or not this object is a valid instance of Inventory
+     * 
+     * @return boolean value denoting validity of object
+     */
+    public boolean isValid() {
+        return valid;
+    }
+
+    /**
+     * Returns this Inventory object as a String
+     * 
+     * @return string of this object
+     */
+    @Override
+    public String toString() {
+        return "Inventory [inventoryId=" + inventoryId + ", itemId=" + itemId + ", locationId=" + locationId
+                + ", quantity=" + quantity + "]";
+    }
+    
+}
--- src/main/resources/yarn_db_init.sql
+++ src/main/resources/yarn_db_init.sql
@@ -1,4 +1,6 @@
 -- Creating and initalizing Yarn Database --
+-- Full database is not yet in use. If desired, comment out appropriate lines 
+-- and statements to create final database.
  
  DROP DATABASE IF EXISTS `Tarico_Yarn_Warehouse`;
  CREATE DATABASE `Tarico_Yarn_Warehouse`;
@@ -14,12 +16,10 @@
 
 CREATE TABLE `item` (
   `item_id` int unsigned NOT NULL AUTO_INCREMENT,
-  `category_id` int unsigned NOT NULL,
+  `category_name` enum('Yarn','Knitting Needles') NOT NULL,
   `price` double NOT NULL,
   `total_quantity` int unsigned DEFAULT NULL,
-  PRIMARY KEY (`item_id`),
-  KEY `category_id_idx` (`category_id`),
-  CONSTRAINT `category_id` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`));
+  PRIMARY KEY (`item_id`));
 
 CREATE TABLE `tarico_yarn_warehouse`.`needle_size` (
   `size_id` INT UNSIGNED NOT NULL,
@@ -58,99 +58,99 @@ CREATE TABLE `tarico_yarn_warehouse`.`yarn` (
   `weight` ENUM("0", "1", "2", "3", "4", "5", "6", "7") NULL,
   PRIMARY KEY (`yarn_item_id`));
 
-CREATE TABLE `tarico_yarn_warehouse`.`address` (
-  `address_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `street_line_one` VARCHAR(45) NOT NULL,
-  `street_line_two` VARCHAR(45) NULL,
-  `state` ENUM("AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY", "MA", "MI", "MN", "MS", "MO") NOT NULL,
-  `city` VARCHAR(45) NOT NULL,
-  `zip` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`address_id`));
+-- CREATE TABLE `tarico_yarn_warehouse`.`address` (
+--   `address_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+--   `street_line_one` VARCHAR(45) NOT NULL,
+--   `street_line_two` VARCHAR(45) NULL,
+--   `state` ENUM("AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY", "MA", "MI", "MN", "MS", "MO") NOT NULL,
+--   `city` VARCHAR(45) NOT NULL,
+--   `zip` INT UNSIGNED NOT NULL,
+--   PRIMARY KEY (`address_id`));
   
 CREATE TABLE `location` (
   `location_id` int unsigned NOT NULL AUTO_INCREMENT,
   `type` enum('Store','Warehouse') NOT NULL,
   `curr_capacity` int unsigned DEFAULT NULL,
   `total_capacity` int unsigned NOT NULL,
-  `address_id` int unsigned NOT NULL,
+  -- `address_id` int unsigned NOT NULL,
   `phone_number` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`location_id`),
-  UNIQUE KEY `phone_number_UNIQUE` (`phone_number`),
-  KEY `address_id_idx` (`address_id`),
-  CONSTRAINT `address_id` FOREIGN KEY (`address_id`) REFERENCES `address` (`address_id`)
+  UNIQUE KEY `phone_number_UNIQUE` (`phone_number`)
+--   KEY `address_id_idx` (`address_id`),
+--   CONSTRAINT `address_id` FOREIGN KEY (`address_id`) REFERENCES `address` (`address_id`)
 );
 
-CREATE TABLE `department` (
-  `department_id` int unsigned NOT NULL,
-  `location_id` int unsigned NOT NULL,
-  `dept_name` varchar(45) NOT NULL,
-  `head` int unsigned DEFAULT NULL,
-  `phone_number` varchar(45) DEFAULT NULL,
-  PRIMARY KEY (`department_id`),
-  UNIQUE KEY `phone_number_UNIQUE` (`phone_number`),
-  KEY `location_id_idx` (`location_id`),
-  CONSTRAINT `location_id` FOREIGN KEY (`location_id`) REFERENCES `location` (`location_id`)
-);
+-- CREATE TABLE `department` (
+--   `department_id` int unsigned NOT NULL,
+--   `location_id` int unsigned NOT NULL,
+--   `dept_name` varchar(45) NOT NULL,
+--   `head` int unsigned DEFAULT NULL,
+--   `phone_number` varchar(45) DEFAULT NULL,
+--   PRIMARY KEY (`department_id`),
+--   UNIQUE KEY `phone_number_UNIQUE` (`phone_number`),
+--   KEY `location_id_idx` (`location_id`),
+--   CONSTRAINT `location_id` FOREIGN KEY (`location_id`) REFERENCES `location` (`location_id`)
+-- );
 
-CREATE TABLE `employee` (
-  `employee_id` int unsigned NOT NULL AUTO_INCREMENT,
-  `first_name` varchar(45) NOT NULL,
-  `last_name` varchar(45) NOT NULL,
-  `deptartment_id` int unsigned DEFAULT NULL,
-  `phone_numnber` varchar(45) DEFAULT NULL,
-  `supervisor` int unsigned NOT NULL,
-  `employee_location` int unsigned NOT NULL,
-  PRIMARY KEY (`employee_id`),
-  KEY `location_id_idx` (`employee_location`),
-  KEY `supervisor_idx` (`supervisor`),
-  CONSTRAINT `employee_location` FOREIGN KEY (`employee_location`) REFERENCES `location` (`location_id`),
-  CONSTRAINT `supervisor` FOREIGN KEY (`supervisor`) REFERENCES `employee` (`employee_id`)
-);
+-- CREATE TABLE `employee` (
+--   `employee_id` int unsigned NOT NULL AUTO_INCREMENT,
+--   `first_name` varchar(45) NOT NULL,
+--   `last_name` varchar(45) NOT NULL,
+--   `deptartment_id` int unsigned DEFAULT NULL,
+--   `phone_numnber` varchar(45) DEFAULT NULL,
+--   `supervisor` int unsigned NOT NULL,
+--   `employee_location` int unsigned NOT NULL,
+--   PRIMARY KEY (`employee_id`),
+--   KEY `location_id_idx` (`employee_location`),
+--   KEY `supervisor_idx` (`supervisor`),
+--   CONSTRAINT `employee_location` FOREIGN KEY (`employee_location`) REFERENCES `location` (`location_id`),
+--   CONSTRAINT `supervisor` FOREIGN KEY (`supervisor`) REFERENCES `employee` (`employee_id`)
+-- );
 
-ALTER TABLE `tarico_yarn_warehouse`.`location` 
-ADD COLUMN `manager_id` INT UNSIGNED NOT NULL AFTER `phone_number`,
-ADD INDEX `manager_id_idx` (`manager_id` ASC) VISIBLE;
-;
-ALTER TABLE `tarico_yarn_warehouse`.`location` 
-ADD CONSTRAINT `manager_id`
-  FOREIGN KEY (`manager_id`)
-  REFERENCES `tarico_yarn_warehouse`.`employee` (`employee_id`)
-  ON DELETE NO ACTION
-  ON UPDATE NO ACTION;
+-- ALTER TABLE `tarico_yarn_warehouse`.`location` 
+-- ADD COLUMN `manager_id` INT UNSIGNED NOT NULL AFTER `phone_number`,
+-- ADD INDEX `manager_id_idx` (`manager_id` ASC) VISIBLE;
+-- ;
+-- ALTER TABLE `tarico_yarn_warehouse`.`location` 
+-- ADD CONSTRAINT `manager_id`
+--   FOREIGN KEY (`manager_id`)
+--   REFERENCES `tarico_yarn_warehouse`.`employee` (`employee_id`)
+--   ON DELETE NO ACTION
+--   ON UPDATE NO ACTION;
 
-CREATE TABLE `orders` (
-  `order_id` int unsigned NOT NULL AUTO_INCREMENT,
-  `time_ordered` datetime NOT NULL,
-  `item_id` int unsigned NOT NULL,
-  `orderer` int unsigned NOT NULL,
-  `shipped` tinyint DEFAULT '0',
-  `quantity` int unsigned NOT NULL,
-  `date_ordered` datetime NOT NULL,
-  PRIMARY KEY (`order_id`),
-  KEY `item_id_idx` (`item_id`),
-  KEY `employee_id_idx` (`orderer`),
-  CONSTRAINT `employee_id` FOREIGN KEY (`orderer`) REFERENCES `employee` (`employee_id`),
-  CONSTRAINT `item_id` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`)
-);
+-- CREATE TABLE `orders` (
+--   `order_id` int unsigned NOT NULL AUTO_INCREMENT,
+--   `time_ordered` datetime NOT NULL,
+--   `item_id` int unsigned NOT NULL,
+--   `orderer` int unsigned NOT NULL,
+--   `shipped` tinyint DEFAULT '0',
+--   `quantity` int unsigned NOT NULL,
+--   `date_ordered` datetime NOT NULL,
+--   PRIMARY KEY (`order_id`),
+--   KEY `item_id_idx` (`item_id`),
+--   KEY `employee_id_idx` (`orderer`),
+--   CONSTRAINT `employee_id` FOREIGN KEY (`orderer`) REFERENCES `employee` (`employee_id`),
+--   CONSTRAINT `item_id` FOREIGN KEY (`item_id`) REFERENCES `item` (`item_id`)
+-- );
 
-CREATE TABLE `shipments` (
-  `shipment_id` int unsigned NOT NULL AUTO_INCREMENT,
-  `order_id` int unsigned NOT NULL,
-  `time_shipped` datetime NOT NULL,
-  `date_shipped` datetime NOT NULL,
-  `time_arrived` datetime DEFAULT NULL,
-  `date_arrived` datetime DEFAULT NULL,
-  `dest_id` int unsigned NOT NULL,
-  `orig_id` int unsigned NOT NULL,
-  `total_price` double NOT NULL,
-  PRIMARY KEY (`shipment_id`),
-  KEY `order_id_idx` (`order_id`),
-  KEY `dest_id_idx` (`dest_id`),
-  KEY `orig_id_idx` (`orig_id`),
-  CONSTRAINT `dest_id` FOREIGN KEY (`dest_id`) REFERENCES `location` (`location_id`),
-  CONSTRAINT `order_id` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
-  CONSTRAINT `orig_id` FOREIGN KEY (`orig_id`) REFERENCES `location` (`location_id`)
-);
+-- CREATE TABLE `shipments` (
+--   `shipment_id` int unsigned NOT NULL AUTO_INCREMENT,
+--   `order_id` int unsigned NOT NULL,
+--   `time_shipped` datetime NOT NULL,
+--   `date_shipped` datetime NOT NULL,
+--   `time_arrived` datetime DEFAULT NULL,
+--   `date_arrived` datetime DEFAULT NULL,
+--   `dest_id` int unsigned NOT NULL,
+--   `orig_id` int unsigned NOT NULL,
+--   `total_price` double NOT NULL,
+--   PRIMARY KEY (`shipment_id`),
+--   KEY `order_id_idx` (`order_id`),
+--   KEY `dest_id_idx` (`dest_id`),
+--   KEY `orig_id_idx` (`orig_id`),
+--   CONSTRAINT `dest_id` FOREIGN KEY (`dest_id`) REFERENCES `location` (`location_id`),
+--   CONSTRAINT `order_id` FOREIGN KEY (`order_id`) REFERENCES `orders` (`order_id`),
+--   CONSTRAINT `orig_id` FOREIGN KEY (`orig_id`) REFERENCES `location` (`location_id`)
+-- );
 
 CREATE TABLE `inventory` (
   `inventory_id` int unsigned NOT NULL AUTO_INCREMENT,
