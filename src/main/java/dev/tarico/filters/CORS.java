4de1d4450b8b3deae897752fc38f343aeea0f76a
tarabarico <tarabarico@gmail.com>
2022-07-29 17:49:18 -0400

    Added comments to URLParser and CORS.

--- src/main/java/dev/tarico/filters/CORS.java
+++ src/main/java/dev/tarico/filters/CORS.java
@@ -1,17 +1,16 @@
 package dev.tarico.filters;
 
 import java.io.IOException;
-
-import javax.servlet.Filter;
-import javax.servlet.FilterChain;
-import javax.servlet.FilterConfig;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
+import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/**
+ * Filter that allows CrossOrigin requests.
+ * 
+ * @author Tara Arico 7.29.2022
+ */
 @WebFilter(urlPatterns = "/*")
 public class CORS implements Filter {
 
@@ -22,6 +21,13 @@ public class CORS implements Filter {
 	@Override
 	public void destroy() {
 	}
+    /**
+     * Adds the appriopriate headers and sends the request on.
+     * 
+     * @param request - original request.
+     * @param response - modified response.
+     * @param chain - FilterChain to send the request on.
+     */
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
--- src/main/java/dev/tarico/services/URLParser.java
+++ src/main/java/dev/tarico/services/URLParser.java
@@ -1,6 +1,17 @@
 package dev.tarico.services;
 
+/**
+ * Helper class that parses the URL.
+ * 
+ * @author Tara Arico 7.29.2022
+ */
 public class URLParser {
+    /**
+     * Parses the given url for the first int.
+     * 
+     * @param url - url to parse.
+     * @return first int value of the url
+     */
     public int extractId(String url) {
         String[] temp = url.split("/");
         return Integer.parseInt(temp[1]);
